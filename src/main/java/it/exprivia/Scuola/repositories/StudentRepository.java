05bb72881f4def5530de085cfdb88965f758f750
Simone Tondo <tr.simonetondo@gmail.com>
2025-12-05 12:02:40 +0100

    Update StudentRepository.java

--- src/main/java/it/exprivia/Scuola/repositories/StudentRepository.java
+++ src/main/java/it/exprivia/Scuola/repositories/StudentRepository.java
@@ -9,6 +9,7 @@ import it.exprivia.Scuola.models.entity.Student;
 @Repository
 public interface StudentRepository extends IPersonRepository<Student> {
 	
+    // ricerca del numero dello studente per evitare la creazione di uno studente con un numero uguale a quello di un'altro
 	Optional<Student> findByStuNum(String stuNum);
 
 }
